ac1fcf62fc6c41e805a0817059308472d2ea09bd
Hugo Emannuel <hugoemannuel@53@gmail.com>
2022-08-04 18:50:19 -0300

    cria logica de desativar o butao

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useMemo } from "react"
 
 const Login = () => {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState ('');
 
-  console.log('email', inputEmail);
-  console.log('passowrd', inputPassword);
+  const isValidButton = useMemo(() => {
+    const regexEmail = /\S+@\S+\.\S+/;
+    if (regexEmail.test(inputEmail) && inputPassword.length < 6 ) {
+      return false;
+    }
+    return true
+  }, [inputEmail, inputPassword])
 
   return (
     <form>
@@ -23,7 +28,7 @@ const Login = () => {
           onChange={({target}) => setInputPassword(target.value)}
          />
       </label>
-      <button type="button">logar</button>
+      <button type="button" disabled={isValidButton}>logar</button>
     </form>
   )
 }
