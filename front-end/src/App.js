e6f23e73635830abba8a77207d160be741d01a11
Hugo Emannuel <hugoemannuel@53@gmail.com>
2022-08-04 19:39:24 -0300

    add react-router-dom e logica de paginaçao

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
