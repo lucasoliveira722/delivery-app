b5a39aa0f24d0d164508e78917c6c2d790f36b60
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-08-04 17:14:29 -0300

    criadas models e migrations

---
+++ back-end/src/database/migrations/20220804194552-salesProducts.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('salesProducts', {
+      sale_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+      },
+      product_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products',
+          key: 'id',
+        }
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('salesProducts');
+
+  }
+};
