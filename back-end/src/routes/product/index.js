b16def5d3f4cb95b411fea6778230a0d88658185
Anderson Leite <andersonleite.dev@gmail.com>
2022-08-08 16:24:41 -0300

    feat(product): create route get /products/:id

--- back-end/src/routes/product/index.js
+++ back-end/src/routes/product/index.js
@@ -4,6 +4,7 @@ const userRouter = express.Router();
 
 const productController = require('../../controllers/product.controller');
 
+userRouter.get('/:id', productController.getById);
 userRouter.get('/', productController.getAll);
 
 module.exports = userRouter;
