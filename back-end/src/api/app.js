0c8f598e75586499c9f14111a91e043ca057fcbf
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-08-09 19:40:36 -0300

    Lint do backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,7 +8,7 @@ const router = require('../routes/index');
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.use(cors())
+app.use(cors());
 app.use(router);
 app.use(errorMiddleware);
 
