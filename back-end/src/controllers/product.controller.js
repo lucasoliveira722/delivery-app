d568088fc0fbddfadbf6cd924b107a5c8a9f51e8
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-08-09 16:11:51 -0300

    fix: função validate jwt

--- back-end/src/controllers/product.controller.js
+++ back-end/src/controllers/product.controller.js
@@ -2,6 +2,7 @@ const ProductService = require('../services/product.service');
 
 const getAll = async (_req, res, _next) => {
   const products = await ProductService.getAll();
+  console.log(_req.role);
   return res.status(200).json(products);
 };
 
--- back-end/src/middlewares/validateJWT.js
+++ back-end/src/middlewares/validateJWT.js
@@ -1,27 +1,23 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { User } = require('../database/models');
 
-const secret = process.env.JWT_SECRET || 'fraseSecreta';
+const secret = process.env.JWT_SECRET || 'your_secret';
 
-module.exports = async (req, res, next) => {
-  const token = req.headers.authorization;
-
-  try {
-    if (!token) {
-      return res.status(401).json({ message: 'Token not found' });
-    }
-
-    const decoded = jwt.verify(token, secret);
-    const user = await User.findOne({ where: { email: decoded.data.email } });
-    if (!user) {
+module.exports = {
+  async validateJWT(req, res, next) {
+    const token = req.headers.authorization;
+  
+    try {
+      if (!token) {
+        return res.status(401).json({ message: 'Token not found' });
+      }
+  
+      const { data } = jwt.verify(token, secret);
+      req.role = data.role;
+  
+      next();
+    } catch (err) {
       return res.status(401).json({ message: 'Expired or invalid token' });
     }
-
-    req.user = user;
-
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Expired or invalid token' });
-  }
+  },
 };
