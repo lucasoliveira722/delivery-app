e70b1dea8637a1fa942871310a422a0bd6dba633
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-08-09 16:40:18 -0300

    refactor: rota products

---
+++ back-end/src/routes/sale/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const saleRouter = express.Router();
+
+const saleController = require('../../controllers/sale.controller');
+const { validateJWT } = require('../../middlewares/validateJWT');
+
+module.exports = saleRouter;
