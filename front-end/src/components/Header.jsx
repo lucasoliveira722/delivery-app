8f3b7565d60bb8e07536cee2517f41095cba06c5
Hugo <hugoemannuel53@gmail.com>
2022-08-10 19:08:55 -0300

    add data teste id no component Header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -3,27 +3,31 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
+    <header data-testid="header">
       <nav style={ { display: 'flex', justifyContent: 'space-between' } }>
         <Link
+          data-testid="customer_products__element-navbar-link-products"
           to="/customer/products"
           style={ { textDecoration: 'none', color: 'black' } }
         >
           <h1>Produtos</h1>
         </Link>
         <Link
+          data-testid="customer_products__element-navbar-link-orders"
           to="/customer/products"
           style={ { textDecoration: 'none', color: 'black' } }
         >
           <h1>Meus pedidos</h1>
         </Link>
         <Link
+          data-testid="customer_products__element-navbar-user-full-name"
           to="/customer/products"
           style={ { textDecoration: 'none', color: 'black' } }
         >
           <h1>Pessoa logada</h1>
         </Link>
         <Link
+          data-testid="customer_products__element-navbar-link-logout"
           to="/customer/products"
           style={ { textDecoration: 'none', color: 'black' } }
         >
