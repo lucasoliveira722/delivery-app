65330483fa94a5112ebdce176b6f7cac2fa331a5
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-08-15 19:54:35 -0300

    feat: tela products finalizada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import GenericContextProvider from './context/GenericContextProvider';
-
+import store from './store';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
@@ -10,12 +11,14 @@ function App() {
   return (
     <GenericContextProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Navigate replace to="/login" /> } />
-          <Route path="/login" element={ <Login /> } />
-          <Route path="/register" element={ <Register /> } />
-          <Route path="customer/products" element={ <Products /> } />
-        </Routes>
+        <Provider store={ store }>
+          <Routes>
+            <Route path="/" element={ <Navigate replace to="/login" /> } />
+            <Route path="/login" element={ <Login /> } />
+            <Route path="/register" element={ <Register /> } />
+            <Route path="customer/products" element={ <Products /> } />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </GenericContextProvider>
   );
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState, useCallback, useContext } from 'react';
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
-import ProductSection from '../components/ProductSection';
+import ProductCard from '../components/ProductCard';
 import GenericContext from '../context/GenericContext';
 import API from '../services/API';
 
 function Products() {
   const { hadleGetItemLocaStorage } = useContext(GenericContext);
-  const [product, setproducts] = useState([]);
+  const [products, setproducts] = useState([]);
   const { token, name } = hadleGetItemLocaStorage('user');
+  const { totalValue, shoppingCart } = useSelector((state) => state.shoppingCart);
+  const navigate = useNavigate();
 
   const getAllProducts = useCallback(async () => {
     try {
@@ -26,7 +29,27 @@ function Products() {
   return (
     <main>
       <Header userName={ name } />
-      <ProductSection products={ product } />
+      <section className="productsSection">
+        {products.map((product, i) => (
+          <ProductCard
+            key={ i }
+            product={ product }
+          />
+        ))}
+      </section>
+      <h2
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        {totalValue.toFixed(2).replace('.', ',')}
+      </h2>
+      <button
+        type="button"
+        data-testid="customer_products__button-cart"
+        onClick={ () => navigate('/customer/checkout') }
+        disabled={ !shoppingCart.some((item) => item.quantity > 0) }
+      >
+        Checkout
+      </button>
     </main>
   );
 }
