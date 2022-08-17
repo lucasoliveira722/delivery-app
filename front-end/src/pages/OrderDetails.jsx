3c8d0ac848cc33537c9510652f6c56860c1fa0d9
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-08-17 18:58:45 -0300

    Requisitos 30 a 32 - Página de detalhes de pedido acessados pelos venderores

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,22 +1,26 @@
-import React, { useCallback, useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useState, useEffect, useContext } from 'react';
+// import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import GenericContext from '../context/GenericContext';
 import API from '../services/API';
 
 function OrderDetails() {
   const [order, setOrder] = useState([]);
-
-  const test = useSelector((state) => console.log(state));
-  console.log(test);
+  const { hadleGetItemLocaStorage } = useContext(GenericContext);
+  // const {iduser} = useParams();
   const getAllSalesById = useCallback(async () => {
-    const id = 1;
+    const { token } = hadleGetItemLocaStorage('user');
+    const id = 3;
+    // IMPLEMENTAR ID DINÂMICO PARA O QUE FOR CLICADO NA PÁGINA ANTERIOR
     try {
-      const response = await API.getSalesById(id);
-      setOrder(response);
+      const response = await API.getSalesById(id, token);
+      setOrder([response]);
     } catch (error) {
       throw new Error(error.message);
     }
-  }, []);
+  }, [hadleGetItemLocaStorage]);
+
+  console.log('order', order);
 
   useEffect(() => {
     getAllSalesById();
@@ -61,52 +65,51 @@ function OrderDetails() {
                 -order-details-label-delivery-status"
               >
                 {`${item.status}`}
-                {/* PEGAR O STATUS DA VENDA Q ESTÁ NO REDUX??? */}
               </h3>
-              {item.itemsSold.map((beer, id) => (
-                <tr key={ id }>
-                  <td
-                    data-testid={ `seller_order_details__element
-                    -order-table-item-number-${id}` }
-                  >
-                    {beer.id}
-                  </td>
-                  <td
-                    data-testid={ `seller_order_details__element
+              {item.products.map((beer, id) => (
+                <>
+                  <tr key={ id }>
+                    <td
+                      data-testid={ `seller_order_details__element
+                      -order-table-item-number-${id}` }
+                    >
+                      {beer.id}
+                    </td>
+                    <td
+                      data-testid={ `seller_order_details__element
                     -order-table-name-${id}` }
-                  >
-                    {beer.name}
-                  </td>
-                  <td
-                    data-testid={ `seller_order_details__element
-                    -order-table-quantity-${id}` }
-                  >
-                    {beer.quantity}
-                  </td>
-                  <td
-                    data-testid={ `seller_order_details__element
-                    -order-table-unit-price-${id}` }
-                  >
-                    {beer.price}
-                  </td>
-                  {/* VERIFICAR COMO PEGAR O VALOR UNITÁRIO DA CERVEJA */}
-                  <td
-                    data-testid={ `seller_order_details__element
-                    -order-table-sub-total-${id}` }
-                  >
-                    {beer.subtotal}
-                  </td>
-                  {/* VERIFICAR SE RECEBEMOS A QUANTIDADE COMPRADA E MULTIPLICAR PLEO VALOR UNITÁRIO */}
-                </tr>
+                    >
+                      {beer.name}
+                    </td>
+                    <td
+                      data-testid={ `seller_order_details__element
+                      -order-table-quantity-${id}` }
+                    >
+                      {beer.SaleProduct.quantity}
+                    </td>
+                    <td
+                      data-testid={ `seller_order_details__element
+                      -order-table-unit-price-${id}` }
+                    >
+                      {beer.price}
+                    </td>
+                    <td
+                      data-testid={ `seller_order_details__element
+                      -order-table-sub-total-${id}` }
+                    >
+                      {(beer.SaleProduct.quantity * beer.price)
+                        .toFixed(2).replace('.', ',')}
+                    </td>
+                  </tr>
+                  <h2>
+                    {`Total: ${item.totalPrice}`}
+                  </h2>
+                </>
               ))}
             </div>
           ))
         }
       </div>
-      <h2>
-        {`Total: ${order.totalValue}`}
-        {/* Recebe do reducer */}
-      </h2>
     </>
   );
 }
